2896c0c191e4cd7b79cc3c32ce99388d37bf18dd
ENG: Malak <110707939+ENGMALAK4112@users.noreply.github.com>
2022-08-15 20:51:53 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,315 @@
+﻿namespace الآلة_الحاسبة2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.n9 = new System.Windows.Forms.TextBox();
+            this.multy = new System.Windows.Forms.TextBox();
+            this.sum = new System.Windows.Forms.TextBox();
+            this.n3 = new System.Windows.Forms.TextBox();
+            this.ac = new System.Windows.Forms.TextBox();
+            this.n6 = new System.Windows.Forms.TextBox();
+            this.n7 = new System.Windows.Forms.TextBox();
+            this.dive = new System.Windows.Forms.TextBox();
+            this.n2 = new System.Windows.Forms.TextBox();
+            this.n5 = new System.Windows.Forms.TextBox();
+            this.sup = new System.Windows.Forms.TextBox();
+            this.n4 = new System.Windows.Forms.TextBox();
+            this.n8 = new System.Windows.Forms.TextBox();
+            this.delete = new System.Windows.Forms.TextBox();
+            this.display = new System.Windows.Forms.TextBox();
+            this.n0 = new System.Windows.Forms.TextBox();
+            this.n1 = new System.Windows.Forms.TextBox();
+            this.point = new System.Windows.Forms.TextBox();
+            this.equl = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // n9
+            // 
+            this.n9.Font = new System.Drawing.Font("Tahoma", 25F);
+            this.n9.Location = new System.Drawing.Point(473, 135);
+            this.n9.Name = "n9";
+            this.n9.Size = new System.Drawing.Size(52, 48);
+            this.n9.TabIndex = 0;
+            this.n9.Text = "9";
+            this.n9.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.n9.TextChanged += new System.EventHandler(this.n9_TextChanged);
+            // 
+            // multy
+            // 
+            this.multy.Font = new System.Drawing.Font("Tahoma", 25F);
+            this.multy.Location = new System.Drawing.Point(725, 203);
+            this.multy.Name = "multy";
+            this.multy.Size = new System.Drawing.Size(52, 48);
+            this.multy.TabIndex = 1;
+            this.multy.Text = "/";
+            this.multy.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.multy.TextChanged += new System.EventHandler(this.multy_TextChanged);
+            // 
+            // sum
+            // 
+            this.sum.Font = new System.Drawing.Font("Tahoma", 25F);
+            this.sum.Location = new System.Drawing.Point(658, 274);
+            this.sum.Name = "sum";
+            this.sum.Size = new System.Drawing.Size(52, 48);
+            this.sum.TabIndex = 2;
+            this.sum.Text = "+";
+            this.sum.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.sum.TextChanged += new System.EventHandler(this.sum_TextChanged);
+            // 
+            // n3
+            // 
+            this.n3.Font = new System.Drawing.Font("Tahoma", 25F);
+            this.n3.Location = new System.Drawing.Point(473, 274);
+            this.n3.Name = "n3";
+            this.n3.Size = new System.Drawing.Size(52, 48);
+            this.n3.TabIndex = 3;
+            this.n3.Text = "3";
+            this.n3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.n3.TextChanged += new System.EventHandler(this.n3_TextChanged);
+            // 
+            // ac
+            // 
+            this.ac.Font = new System.Drawing.Font("Tahoma", 25F);
+            this.ac.Location = new System.Drawing.Point(658, 135);
+            this.ac.Multiline = true;
+            this.ac.Name = "ac";
+            this.ac.Size = new System.Drawing.Size(119, 48);
+            this.ac.TabIndex = 4;
+            this.ac.Text = "AC";
+            this.ac.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.ac.TextChanged += new System.EventHandler(this.ac_TextChanged);
+            // 
+            // n6
+            // 
+            this.n6.Font = new System.Drawing.Font("Tahoma", 25F);
+            this.n6.Location = new System.Drawing.Point(473, 203);
+            this.n6.Name = "n6";
+            this.n6.Size = new System.Drawing.Size(52, 48);
+            this.n6.TabIndex = 5;
+            this.n6.Text = "6";
+            this.n6.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.n6.TextChanged += new System.EventHandler(this.n6_TextChanged);
+            // 
+            // n7
+            // 
+            this.n7.Font = new System.Drawing.Font("Tahoma", 25F);
+            this.n7.Location = new System.Drawing.Point(531, 203);
+            this.n7.Name = "n7";
+            this.n7.Size = new System.Drawing.Size(52, 48);
+            this.n7.TabIndex = 6;
+            this.n7.Text = "7";
+            this.n7.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.n7.TextChanged += new System.EventHandler(this.n7_TextChanged);
+            // 
+            // dive
+            // 
+            this.dive.Font = new System.Drawing.Font("Tahoma", 25F);
+            this.dive.Location = new System.Drawing.Point(658, 203);
+            this.dive.Name = "dive";
+            this.dive.Size = new System.Drawing.Size(52, 48);
+            this.dive.TabIndex = 7;
+            this.dive.Text = "*";
+            this.dive.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.dive.TextChanged += new System.EventHandler(this.dive_TextChanged);
+            // 
+            // n2
+            // 
+            this.n2.Font = new System.Drawing.Font("Tahoma", 25F);
+            this.n2.Location = new System.Drawing.Point(589, 337);
+            this.n2.Name = "n2";
+            this.n2.Size = new System.Drawing.Size(52, 48);
+            this.n2.TabIndex = 8;
+            this.n2.Text = "2";
+            this.n2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.n2.TextChanged += new System.EventHandler(this.n2_TextChanged);
+            // 
+            // n5
+            // 
+            this.n5.Font = new System.Drawing.Font("Tahoma", 25F);
+            this.n5.Location = new System.Drawing.Point(589, 274);
+            this.n5.Name = "n5";
+            this.n5.Size = new System.Drawing.Size(52, 48);
+            this.n5.TabIndex = 9;
+            this.n5.Text = "5";
+            this.n5.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.n5.TextChanged += new System.EventHandler(this.n5_TextChanged);
+            // 
+            // sup
+            // 
+            this.sup.Font = new System.Drawing.Font("Tahoma", 25F);
+            this.sup.Location = new System.Drawing.Point(725, 274);
+            this.sup.Name = "sup";
+            this.sup.Size = new System.Drawing.Size(52, 48);
+            this.sup.TabIndex = 10;
+            this.sup.Text = "-";
+            this.sup.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.sup.TextChanged += new System.EventHandler(this.sup_TextChanged);
+            // 
+            // n4
+            // 
+            this.n4.Font = new System.Drawing.Font("Tahoma", 25F);
+            this.n4.Location = new System.Drawing.Point(531, 274);
+            this.n4.Name = "n4";
+            this.n4.Size = new System.Drawing.Size(52, 48);
+            this.n4.TabIndex = 11;
+            this.n4.Text = "4";
+            this.n4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.n4.TextChanged += new System.EventHandler(this.n4_TextChanged);
+            // 
+            // n8
+            // 
+            this.n8.Font = new System.Drawing.Font("Tahoma", 25F);
+            this.n8.Location = new System.Drawing.Point(589, 203);
+            this.n8.Name = "n8";
+            this.n8.Size = new System.Drawing.Size(52, 48);
+            this.n8.TabIndex = 12;
+            this.n8.Text = "8";
+            this.n8.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.n8.TextChanged += new System.EventHandler(this.n8_TextChanged);
+            // 
+            // delete
+            // 
+            this.delete.Font = new System.Drawing.Font("Tahoma", 20F);
+            this.delete.Location = new System.Drawing.Point(531, 135);
+            this.delete.Multiline = true;
+            this.delete.Name = "delete";
+            this.delete.Size = new System.Drawing.Size(110, 48);
+            this.delete.TabIndex = 13;
+            this.delete.Text = "DEL";
+            this.delete.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.delete.TextChanged += new System.EventHandler(this.delete_TextChanged);
+            // 
+            // display
+            // 
+            this.display.Font = new System.Drawing.Font("Tahoma", 15F);
+            this.display.Location = new System.Drawing.Point(473, 24);
+            this.display.Name = "display";
+            this.display.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.display.Size = new System.Drawing.Size(304, 32);
+            this.display.TabIndex = 14;
+            // 
+            // n0
+            // 
+            this.n0.Font = new System.Drawing.Font("Tahoma", 25F);
+            this.n0.Location = new System.Drawing.Point(473, 337);
+            this.n0.Name = "n0";
+            this.n0.Size = new System.Drawing.Size(52, 48);
+            this.n0.TabIndex = 15;
+            this.n0.Text = "0";
+            this.n0.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.n0.TextChanged += new System.EventHandler(this.n0_TextChanged);
+            // 
+            // n1
+            // 
+            this.n1.Font = new System.Drawing.Font("Tahoma", 25F);
+            this.n1.Location = new System.Drawing.Point(531, 337);
+            this.n1.Name = "n1";
+            this.n1.Size = new System.Drawing.Size(52, 48);
+            this.n1.TabIndex = 16;
+            this.n1.Text = "1";
+            this.n1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.n1.TextChanged += new System.EventHandler(this.n1_TextChanged);
+            // 
+            // point
+            // 
+            this.point.Font = new System.Drawing.Font("Tahoma", 25F);
+            this.point.Location = new System.Drawing.Point(658, 337);
+            this.point.Name = "point";
+            this.point.Size = new System.Drawing.Size(52, 48);
+            this.point.TabIndex = 17;
+            this.point.Text = ".";
+            this.point.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.point.TextChanged += new System.EventHandler(this.point_TextChanged);
+            // 
+            // equl
+            // 
+            this.equl.Font = new System.Drawing.Font("Tahoma", 25F);
+            this.equl.Location = new System.Drawing.Point(725, 337);
+            this.equl.Name = "equl";
+            this.equl.Size = new System.Drawing.Size(52, 48);
+            this.equl.TabIndex = 18;
+            this.equl.Text = "=";
+            this.equl.UseVisualStyleBackColor = true;
+            this.equl.Click += new System.EventHandler(this.equl_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(793, 450);
+            this.Controls.Add(this.equl);
+            this.Controls.Add(this.point);
+            this.Controls.Add(this.n1);
+            this.Controls.Add(this.n0);
+            this.Controls.Add(this.display);
+            this.Controls.Add(this.delete);
+            this.Controls.Add(this.n8);
+            this.Controls.Add(this.n4);
+            this.Controls.Add(this.sup);
+            this.Controls.Add(this.n5);
+            this.Controls.Add(this.n2);
+            this.Controls.Add(this.dive);
+            this.Controls.Add(this.n7);
+            this.Controls.Add(this.n6);
+            this.Controls.Add(this.ac);
+            this.Controls.Add(this.n3);
+            this.Controls.Add(this.sum);
+            this.Controls.Add(this.multy);
+            this.Controls.Add(this.n9);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox n9;
+        private System.Windows.Forms.TextBox multy;
+        private System.Windows.Forms.TextBox sum;
+        private System.Windows.Forms.TextBox n3;
+        private System.Windows.Forms.TextBox ac;
+        private System.Windows.Forms.TextBox n6;
+        private System.Windows.Forms.TextBox n7;
+        private System.Windows.Forms.TextBox dive;
+        private System.Windows.Forms.TextBox n2;
+        private System.Windows.Forms.TextBox n5;
+        private System.Windows.Forms.TextBox sup;
+        private System.Windows.Forms.TextBox n4;
+        private System.Windows.Forms.TextBox n8;
+        private System.Windows.Forms.TextBox delete;
+        private System.Windows.Forms.TextBox display;
+        private System.Windows.Forms.TextBox n0;
+        private System.Windows.Forms.TextBox n1;
+        private System.Windows.Forms.TextBox point;
+        private System.Windows.Forms.Button equl;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace الآلة_الحاسبة2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void n0_TextChanged(object sender, EventArgs e)
+        {
+              n0.Text= display.Text;
+            n0.Text="0";
+            display.Text = n0.Text;
+        }
+
+        private void n9_TextChanged(object sender, EventArgs e)
+        {
+            display.Text = n9.Text;
+            double n = Convert.ToDouble(n9.Text);
+            n9.Text = (9).ToString();
+
+        }
+
+        private void point_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void n1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void delete_TextChanged(object sender, EventArgs e)
+        {
+            display.Text = delete.Text;
+            display.Clear();
+        }
+
+        private void n8_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void n4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void sup_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void n5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void n2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dive_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void n7_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void n6_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ac_TextChanged(object sender, EventArgs e)
+        {
+            display.Text = ac.Text;
+            display.Hide();
+        }
+
+        private void n3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void sum_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void multy_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void equl_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
